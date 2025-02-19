dd0054172f852d28cb18c5beb1ac8c1df67b0034
JL Bautista <jl.bautistac@gmail.com>
2025-02-19 15:12:36 -0600

    Add methods to usersController: findOne, update, delete

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,3 +19,33 @@ exports.findAll = async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 };
+
+// Get a user by id
+exports.findOne = async (req, res) => {
+    try {
+        const user = await User.findOne(req.params.id);
+        res.json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Update a user by id
+exports.update = async (req, res) => {
+    try {
+        const user = await User.update(req.params.id, req.body);
+        res.json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Delete a user by id
+exports.delete = async (req, res) => {
+    try {
+        await User.delete(req.params.id);
+        res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
