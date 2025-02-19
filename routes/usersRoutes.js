5289ceff9df5ad62b57644c6807118d754f34838
JL Bautista <jl.bautistac@gmail.com>
2025-02-19 15:16:13 -0600

    Add routes to usersRoutes: get by id, put and delete

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,5 +4,8 @@ const usersController = require('../controllers/usersController');
 
 router.post('/', usersController.create);
 router.get('/', usersController.findAll);
+router.get('/:id', usersController.findOne);
+router.put('/:id', usersController.update);
+router.delete('/:id', usersController.delete);
 
 module.exports = router;
